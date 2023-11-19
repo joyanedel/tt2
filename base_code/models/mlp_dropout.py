dfae5e053c8bc1d7974b74c67cce2a5149d8109e
Jesús Oyanedel Espinoza <jesusoyanedelespinoza@MacBook-Air-de-Jesus.local>
2023-11-19 15:52:36 -0300

    Add ewc from kirkpatrick and cumulative strategies

---
+++ base_code/models/mlp_dropout.py
@@ -0,0 +1,23 @@
+from torch import nn
+
+
+class MLPDropout(nn.Module):
+    def __init__(self, n_classes: int, n_channels: int, width: int, height: int) -> None:
+        super().__init__()
+        self.sequential = nn.Sequential(
+            nn.Flatten(),
+            nn.Dropout(0.2),
+
+            nn.Linear(in_features=n_channels*width*height, out_features=400), # 1*28*28 (784 x 100)
+            nn.ReLU(),
+            nn.Dropout(0.5),
+
+            nn.Linear(in_features=400, out_features=400), # (100 x 100)
+            nn.ReLU(),
+            nn.Dropout(0.5),
+        )
+        self.output = nn.Linear(in_features=400, out_features=n_classes) # (100 x 10)
+    
+    def forward(self, x):
+        x = self.sequential(x)
+        return self.output(x)
