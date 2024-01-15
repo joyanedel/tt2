a74048d4c9e0585cafbe73857c88b8490a10d934
joyanedel <joyanede@alumnos.inf.utfsm.cl>
2024-01-15 20:42:28 -0300

    Last commit

---
+++ base_code/seed.py
@@ -0,0 +1,16 @@
+import numpy as np
+import random
+import torch
+
+
+def set_seed(seed):
+    if seed is None:
+        return
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    if torch.cuda.is_available():
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.enabled = True
+        torch.backends.cudnn.benchmark = False
